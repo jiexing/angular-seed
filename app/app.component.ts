280931beec7f6fb192f3055775aee322fad4bb29
DESKTOP-11LGPP8\jiexi <jie.xing@gmail.com>
2018-07-14 08:47:25 -0400

    angular seed - webpack

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div>
+      angular seed
+    </div>
+  `
+})
+export class AppComponent {
+}
